8fefc283799e434773bc98653c2e7d1b40346cba
Igor Fernandes <igormedfer@gmail.com>
2023-02-23 20:54:58 -0300

    initial commit

---
+++ Clima/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Clima
+//
+//  Created by Igor Fernandes on 23/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
